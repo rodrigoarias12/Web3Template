a824252b775ff80e23df210bd9d762e6179ca4d9
rodrigoarias12 <rodrigogonzaloarias@gmail.com>
2022-11-02 00:33:36 -0300

    Update _app.tsx

--- packages/dapp/pages/_app.tsx
+++ packages/dapp/pages/_app.tsx
@@ -1,6 +1,44 @@
+import '@rainbow-me/rainbowkit/styles.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
+const { chains, provider } = configureChains(
+  [
+    chain.polygonMumbai,
+  ],
+  [
+  
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/polygon_mumbai",
+        };
+      },
+    }),
+    publicProvider(),
+   
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider showRecentTransactions={true} chains={chains}>
+      <Component {...pageProps} />
+       {/* <Analytics /> */}
+    </RainbowKitProvider>
+  </WagmiConfig>
+  )
 }
