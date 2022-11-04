62cf07ed5ce1118445153c1c168695712cacb0a7
rodrigoarias12 <rodrigogonzaloarias@gmail.com>
2022-11-04 17:41:04 -0300

    update  "@rainbow-me/rainbowkit": "^0.7.1",

--- packages/dapp/pages/index.tsx
+++ packages/dapp/pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import '@rainbow-me/rainbowkit/styles.css';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount } from "wagmi";
-import { useEffect} from "react";
+import { useEffect } from "react";
 export default function Home() {
 
 
@@ -23,61 +23,61 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <ConnectButton
+        <ConnectButton
           showBalance={true}
           accountStatus={{
             smallScreen: "avatar",
             largeScreen: "full",
           }}
         />
-         {/* {!isConnected ? (
-        <div className="text-center font-bold text-xl m-8">
-          Please connect to wallet
-        </div>
-      ) : (
-        <> */}
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        {!isConnected ? (
+          <div className="text-center font-bold text-xl m-8">
+            Please connect to wallet
+          </div>
+        ) : (
+          <>
+            <h1 className={styles.title}>
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <p className={styles.description}>
+              Get started by editing{' '}
+              <code className={styles.code}>pages/index.tsx</code>
             </p>
-          </a>
-        </div>
-        {/* </>
-      )} */}
+
+            <div className={styles.grid}>
+              <a href="https://nextjs.org/docs" className={styles.card}>
+                <h2>Documentation &rarr;</h2>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className={styles.card}>
+                <h2>Learn &rarr;</h2>
+                <p>Learn about Next.js in an interactive course with quizzes!</p>
+              </a>
+
+              <a
+                href="https://github.com/vercel/next.js/tree/canary/examples"
+                className={styles.card}
+              >
+                <h2>Examples &rarr;</h2>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.card}
+              >
+                <h2>Deploy &rarr;</h2>
+                <p>
+                  Instantly deploy your Next.js site to a public URL with Vercel.
+                </p>
+              </a>
+            </div>
+          </>
+        )}
       </main>
 
       <footer className={styles.footer}>
